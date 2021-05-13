7ef7e0163477175b334adf3ca6887920fcf95bb0
Restyled.io <commits@restyled.io>
2021-05-13 06:58:25 +0000

    Restyled by autopep8

--- modules/cc.py
+++ modules/cc.py
@@ -48,13 +48,14 @@ class CC(commands.Cog, name="지식 및 배우기"):
             if response.voted:
                 embed = discord.Embed(
                     title="정말로 미야에게 이렇게 가르칠까요?",
-                    description=
-                    f"등록되면 미야가 `{word}`라고 물어봤을 때```{value}```(이)라고 답할거에요.\n \n*부적절한 어휘 및 답변의 경우 예고 없이 삭제될 수 있어요.*",
+                    description=f"등록되면 미야가 `{word}`라고 물어봤을 때```{value}```(이)라고 답할거에요.\n \n*부적절한 어휘 및 답변의 경우 예고 없이 삭제될 수 있어요.*",
                     color=0x5FE9FF,
                     timestamp=datetime.datetime.utcnow(),
                 )
-                embed.set_author(name="가르치기", icon_url=self.miya.user.avatar_url)
-                embed.set_thumbnail(url=ctx.author.avatar_url_as(static_format="png", size=2048))
+                embed.set_author(
+                    name="가르치기", icon_url=self.miya.user.avatar_url)
+                embed.set_thumbnail(url=ctx.author.avatar_url_as(
+                    static_format="png", size=2048))
                 embed.set_footer(text="미야를 똑똑하게 만들기 프로젝트")
                 msg = await ctx.reply(embed=embed)
                 await msg.add_reaction("<:cs_yes:659355468715786262>")
@@ -80,13 +81,14 @@ class CC(commands.Cog, name="지식 및 배우기"):
                         )
                         embed = discord.Embed(
                             title="가르쳐주셔서 고마워요!",
-                            description=
-                            f"이제 `{word}`에 이렇게 답할거에요:\n```{value}```",
+                            description=f"이제 `{word}`에 이렇게 답할거에요:\n```{value}```",
                             color=0x5FE9FF,
                             timestamp=datetime.datetime.utcnow(),
                         )
-                        embed.set_author(name="가르치기", icon_url=self.miya.user.avatar_url)
-                        embed.set_thumbnail(url=ctx.author.avatar_url_as(static_format="png", size=2048))
+                        embed.set_author(
+                            name="가르치기", icon_url=self.miya.user.avatar_url)
+                        embed.set_thumbnail(url=ctx.author.avatar_url_as(
+                            static_format="png", size=2048))
                         embed.set_footer(text="미야를 똑똑하게 만들기 프로젝트")
                         await msg.edit(embed=embed)
                     else:
